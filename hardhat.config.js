87dae881fb17f4f0b01adfa07e0ade1eac815256
Rediet21 <redogirma@gmail.com>
2024-02-07 22:41:20 +0300

    sepolia connection and adding ALchemy API

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+defaultNetwork: "hardhat",
+networks: {
+  hardhat: {
+    chainId: 1337
+  },
+  sepolia: {
+    url: "https://eth-sepolia.g.alchemy.com/v2/WPft7dHoQeX-Lrj26L5m3lu-5c4I8-GO",
+    accounts: [process.env.PRIVATE_KEY],
+  }
+},
+
   solidity: "0.8.19",
 };
