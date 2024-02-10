96cd0da045c4add9f0d79bbb68e3e906890155e6
Rediet21 <redogirma@gmail.com>
2024-02-10 11:45:09 +0300

    deploying contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,18 @@
 const { ethers } = require("hardhat");
 
+const rewardsTokenAddress = "0x6db049243Cd27d3b46b76adB38eD42c3b84CB83e"; // Replace with the ERC20 token contract address
+const employerAddress = "0xBD6d01fa990348Fd962870fbf7DeD4C0725f85D7"; // Replace with the employer's address
+
+
 async function main() {
   // Deploying the DriverCompliance contract
   const DriverCompliance = await ethers.getContractFactory("DriverCompliance");
   const driverCompliance = await DriverCompliance.deploy(
     /* Pass the addresses of rewardsToken and employer here */
-    "<rewardsTokenAddress>",
-    "<employerAddress>"
+    rewardsTokenAddress,
+    employerAddress
   );
-  await driverCompliance.deployed();
+  //await driverCompliance.deployed();
   console.log("DriverCompliance contract deployed to:", driverCompliance.address);
 }
 
