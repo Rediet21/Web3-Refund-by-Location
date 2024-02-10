9b5adf29af4f73c6f3e790520f0145b16fb4e02b
Rediet21 <redogirma@gmail.com>
2024-02-10 03:19:51 +0300

    debug smart contract

--- contracts/newcontract.sol
+++ contracts/newcontract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 //importing ERC20.sol file from the openZeppelin library
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
@@ -9,8 +9,8 @@ contract DriverCompliance {
         string latitude;
         string longitude;
         uint256 timestamp;
-        uint256 tokenBalance;
-        bool isRegistered;
+        //uint256 tokenBalance;
+        //bool isRegistered;
     }
     //enum is user-defined data & representing diffrent level of compliance 
     enum ComplianceLevel {Bad, OK, Good, Excellent}
@@ -22,12 +22,13 @@ contract DriverCompliance {
     uint256 public salary = 3 ether;
     uint256 public bonus = 2 ether;
     uint256 public rewardPer_Compliance = 1 ether;
+    uint256 public Location_stated;
 
     //mapping the drivers address to location and complianceLevel 
     mapping(address => Location) public driverLocations;
     mapping(address => ComplianceLevel) public driverCompliance;
 
-    //events are ways to emit and login information about specific occurence
+    
     //events are a way to emit and log information about specific occurrences or 
     //actions within a contract. They are useful for notifying external systems or 
     //front-end applications about important events that occurred during contract execution.
@@ -43,11 +44,13 @@ contract DriverCompliance {
         employer = _employer;
     }
     // This function is used by the employer to intiate the work by setting longtude, lattitude and the time stamp
-    function startWork() external payable {
+    function startWork(string memory longitude_set, string memory latitude_set, uint256 timestamp_set) external payable {
         require(msg.sender == employer, "only the employer can initiate the process");
         require(msg.value == lock_amount, "Please lock the specified amount of ETH.");
-        driverLocations[msg.sender] = Location("", "", 0);
+        driverLocations[msg.sender] = Location(longitude_set, latitude_set, timestamp_set);
+
         driverCompliance[msg.sender] = ComplianceLevel.Bad;
+        //put the location and time stamp in to variable inorder to compare them with the one sent from the driver & he should set the time stamp to zero
         emit StartWorking(msg.sender);
         }
     //this function depends on function below
@@ -61,36 +64,34 @@ contract DriverCompliance {
     }
     // what does the variable driver stands for
     function checkCompliance(address driver) internal view returns (ComplianceLevel) {
-        Location memory location = driverLocations[driver];
-        uint256 currentTimestamp = block.timestamp;
+         Location memory employerLocation = driverLocations[employer];
+         Location memory driverLocation = driverLocations[driver];
 
-        if (compareStrings(location.latitude, "specified_latitude") && compareStrings(location.longitude, "specified_longitude")){
-            if (location.timestamp == block.timestamp) {
+        if (compareStrings(employerLocation.latitude, driverLocation.latitude) &&
+        compareStrings(employerLocation.longitude, driverLocation.longitude)) {
+            if (employerLocation.timestamp == driverLocation.timestamp ) {
                 return ComplianceLevel.Excellent;
-            } else if  (location.timestamp > currentTimestamp - 1 days) { 
+            } else {
                 return ComplianceLevel.Good;
-            }  else { 
-                return ComplianceLevel.Bad;
             }
-         }
-         else {
+        }
+        else {
             return ComplianceLevel.Bad;
-         }
-}
- function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+            }
+    }
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
-function paySalary() external {
-    ComplianceLevel compliance = driverCompliance[msg.sender];
-    require(compliance != ComplianceLevel.Bad, "Driver has not fulfilled any compliance criteria.");
+    // function that should be cinnected with the ehtereum payment fot the transaction
+    function paySalary() external {
+        ComplianceLevel compliance = driverCompliance[msg.sender];
+        require(compliance != ComplianceLevel.Bad, "Driver has not fulfilled any compliance criteria.");
 
-    if (compliance == ComplianceLevel.Excellent) {
-        rewardsToken.transferFrom(employer, msg.sender, rewardPer_Compliance * 5);
-    } else if (compliance == ComplianceLevel.Good) {
-        rewardsToken.transferFrom(employer, msg.sender, rewardPer_Compliance * 3);
-    } else if (compliance == ComplianceLevel.OK) {
-        rewardsToken.transferFrom(employer, msg.sender, rewardPer_Compliance * 2);
-    }
+        if (compliance == ComplianceLevel.Excellent) {
+            rewardsToken.transferFrom(employer, msg.sender, rewardPer_Compliance * 5);
+        } else if (compliance == ComplianceLevel.Good) {
+            rewardsToken.transferFrom(employer, msg.sender, rewardPer_Compliance * 3);
+        } 
 
     payable(msg.sender).transfer(salary);
     emit RewardPaid(msg.sender, rewardPer_Compliance * uint256(compliance));
