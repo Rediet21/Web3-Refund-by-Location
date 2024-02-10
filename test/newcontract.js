ba4c76af33951b92b46c62551eb388622b915cd2
Rediet21 <redogirma@gmail.com>
2024-02-10 12:07:04 +0300

    assigning values for test

--- test/newcontract.js
+++ test/newcontract.js
@@ -1,4 +1,7 @@
 const { ethers } = require("hardhat");
+const { utils } = ethers;
+const rewardsTokenAddress = "0x6db049243Cd27d3b46b76adB38eD42c3b84CB83e"; // Replace with the ERC20 token contract address
+const employerAddress = "0xBD6d01fa990348Fd962870fbf7DeD4C0725f85D7"; // Replace with the employer's address
 
 async function main() {
   // Deploying the DriverCompliance contract
@@ -9,14 +12,15 @@ async function main() {
     //employer adress as registered in ethereum
     employerAddress
   );
-  await driverCompliance.deployed();
+  //await driverCompliance.deployed();
   console.log("DriverCompliance contract deployed to:", driverCompliance.address);
 
   // Testing the contract functionality
   const [deployer, driver] = await ethers.getSigners();
 
   // Start work by the employer
-  const lockAmount = ethers.utils.parseEther("5");
+  //const lockAmount = ethers.utils.parseEther("5");
+  const lockAmount = utils.parseEther("5");
   await driverCompliance.connect(deployer).startWork("longitude", "latitude", Math.floor(Date.now() / 1000), { value: lockAmount });
 
   // Set driver's location
